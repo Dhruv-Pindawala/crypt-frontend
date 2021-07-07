ad2a53d0dc26ceb6ae6bfb5ceb87a33b89599036
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-07-07 23:52:34 +0530

    basic store and storeprovider

--- src/stateManagement/store.js
+++ src/stateManagement/store.js
@@ -1,10 +1,22 @@
 import React, {createContext, useReducer} from 'react';
 
-const store = createContext({});
+const reduceReducers = (...reducers) => (prevState, value, ...args) => {
+    reducers.reduce(
+        (newState, reducer) => reducer(newState, value, ...args), prevState
+    );
+};
+
+const combinedReducers = reduceReducers();
+
+const initialState = {};
+
+const store = createContext(initialState);
 const {Provider} = store;
 
 const StoreProvider = ({children}) => {
-    const [state, dispatch] = useReducer(() => {}, {});
+    const [state, dispatch] = useReducer(combinedReducers, initialState);
 
     return <Provider value={{state, dispatch}}>{children}</Provider>;
 };
+
+export { store, StoreProvider };
