305f691e207f5d6009868f859042175c610edd64
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-07-10 13:46:32 +0530

    basic login page

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import eyeopen from '../assets/eyeopen.png';
+import eyeclose from '../assets/eyeclose.png';
+import {Link} from 'react-router-dom';
 
 const Login = (props) => {
-    return <div>Login</div>;
+    return (
+        <div className='loginContainer'>
+            <div className='inner'>
+                <div className='logo'>CRYPT</div>
+                <div className='title'>Sign in</div>
+                <form>
+                    <input className='input-field' placeholder='Username' />
+                    <div className='input-container'>
+                        <input className='input-field' placeholder='Password' type='password' />
+                        <img src={eyeopen} />
+                        <img src={eyeclose} />
+                    </div>
+                    <div className='flex justify-end'>
+                        <Link to='/'>Forgot Password</Link>
+                    </div>
+                    <button type='submit'>Login</button>
+                </form>
+            </div>
+        </div>
+    )
 };
 
 export default Login;
