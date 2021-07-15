3000803fe9f42b63383b714239f438937770f3fb
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-07-15 20:51:21 +0530

    updates to profile modal

---
+++ src/urls.js
@@ -0,0 +1,7 @@
+const BASE_URL = "http://localhost:3000/";
+
+export const LOGIN_URL = BASE_URL + "user/login";
+export const REGISTER_URL = BASE_URL + "user/register";
+export const PROFILE_URL = BASE_URL + "user/profile";
+export const ME_URL = BASE_URL + "user/me";
+export const REFERESH_URL = BASE_URL + "user/refresh";
