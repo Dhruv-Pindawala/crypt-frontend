fe3a3ea89cf4ed3bd8feb488bb552e9e1f600445
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-07-15 20:39:07 +0530

    user profile

--- src/Pages/homeComponents.js
+++ src/Pages/homeComponents.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import close from "../assets/close.png";
+import edit from "../assets/edit.png";
 
-const UserMain = (props) => {
+export const UserMain = (props) => {
     return (
         <div className='flex align-center justify-between userMain'>
             <UserAvatar isV2 />
@@ -9,7 +11,7 @@ const UserMain = (props) => {
     );
 };
 
-const UserAvatar = (props) => {
+export const UserAvatar = (props) => {
     return (
         <div className={`userAvatar ${props.isV2 ? "version2":""}`}>
             <div className = 'imageCon' style={{backgroundImage: `url("https://cdn.pixabay.com/photo/2017/05/25/21/26/bird-feeder-2344414__340.jpg")`,}} />
@@ -21,7 +23,7 @@ const UserAvatar = (props) => {
     );
 };
 
-const ChatBubble = (props) => {
+export const ChatBubble = (props) => {
     return (
         <div className={`chatbubbleCon ${props.bubbleType}`}>
             <div className='chatbubble'>
@@ -31,3 +33,51 @@ const ChatBubble = (props) => {
         </div>
     );
 };
+
+export const ProfileModal = (props) => {
+    return (
+      <div className={`modalContain ${props.visible ? "open" : ""}`}>
+        <div className="content-inner">
+          <div className="header">
+            <div className="title">Update Profile</div>
+            <img src={close} onClick={props.close} />
+          </div>
+          <div className="content">
+            <div className="inner">
+              <div className="leftHook">
+                <div
+                  className="imageCon"
+                  style={{
+                    backgroundImage: `url("https://cdn.pixabay.com/photo/2017/05/25/21/26/bird-feeder-2344414__340.jpg")`,
+                  }}
+                />
+                <div className="point">
+                  Change Picture
+                  <img src={edit} />
+                </div>
+              </div>
+              <div className="dataInput">
+                <label>
+                  <span>First name</span>
+                  <input />
+                </label>
+                <label>
+                  <span>Last name</span>
+                  <input />
+                </label>
+                <label>
+                  <span>Caption</span>
+                  <input />
+                </label>
+                <label>
+                  <span>About</span>
+                  <textarea />
+                </label>
+              </div>
+            </div>
+            <button>Update</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
